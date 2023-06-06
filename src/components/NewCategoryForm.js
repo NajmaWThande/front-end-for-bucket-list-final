5e9295b586c2d4665605f1653f66e83597f76780
kk <+>
2023-06-06 22:40:25 +0300

    Added submit button

--- src/components/NewCategoryForm.js
+++ src/components/NewCategoryForm.js
@@ -4,6 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { Box, TextField, Button, FormControl} from '@mui/material'
 
+const SubmitButton = styled(Button)({
+    backgroundColor: 'paper',
+    color: 'white',
+    fontSize: '1.5rem',
+    border: '1px solid white',
+    height: 56,
+    width: 100  
+  })
 const NewCategoryForm = ({onHandleAddCategory, onSetIsAddingCategory, categories}) => {
     const navigate = useNavigate();  
   
