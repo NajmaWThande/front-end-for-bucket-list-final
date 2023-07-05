a03bfc79e1a790780e4183e4e791b8207d437b48
Najma <najmathande@gmail.com>
2023-07-06 02:06:17 +0300

    added package.json

--- src/Global.js
+++ src/Global.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export const dataURL = 'http://localhost:4000/bucket_list';
-=======
-export const dataURL = 'http://localhost:9292/bucket_list';
->>>>>>> 31f00913de60d3a7a6fa7115fa652e24d45f9163
 
 export const headers = {
     "Accept": "application/json",
